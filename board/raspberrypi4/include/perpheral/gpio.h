a998fe9d3f1ee1d75895d725bf50c76ebf2b93c1
Gordon <bkgr4949p@icloud.com>
2025-07-16 18:48:31 +0900

    Revise the GPIO peripheral-related code in the gpio.h and gpio.c files due to changes in register access.

--- board/raspberrypi4/include/perpheral/gpio.h
+++ board/raspberrypi4/include/perpheral/gpio.h
@@ -4,14 +4,14 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define GPIO_FUNC_INPUT     ((uint32_t)0x00)
-#define GPIO_FUNC_OUTPUT    ((uint32_t)0x01)
-#define GPIO_FUNC_ALT0      ((uint32_t)0x04)
-#define GPIO_FUNC_ALT1      ((uint32_t)0x05)
-#define GPIO_FUNC_ALT2      ((uint32_t)0x06)
-#define GPIO_FUNC_ALT3      ((uint32_t)0x07)
-#define GPIO_FUNC_ALT4      ((uint32_t)0x03)
-#define GPIO_FUNC_ALT5      ((uint32_t)0x02)
+#define GPIO_FUNC_INPUT     (0x00)
+#define GPIO_FUNC_OUTPUT    (0x01)
+#define GPIO_FUNC_ALT0      (0x04)
+#define GPIO_FUNC_ALT1      (0x05)
+#define GPIO_FUNC_ALT2      (0x06)
+#define GPIO_FUNC_ALT3      (0x07)
+#define GPIO_FUNC_ALT4      (0x03)
+#define GPIO_FUNC_ALT5      (0x02)
 
 void GPIO_SelectFunction(uint32_t pin, uint32_t function);
 void GPIO_SetOutput(uint32_t pin,bool value);
--- board/raspberrypi4/src/perpheral/gpio/gpio.c
+++ board/raspberrypi4/src/perpheral/gpio/gpio.c
@@ -4,21 +4,16 @@
 #include "memory.h"
 
 void GPIO_SelectFunction(uint32_t pin, uint32_t function){
+    volatile uint32_t * GPSEL_base = BCM2711_GPIO_GPFSEL0;
+
     uint32_t regIndex = (pin / 10);
     uint32_t shiftCount = (pin % 10) * 3;
 
-    uint32_t oldGPFSEL = READ_REG_32(BCM2711_GPFSEL0 + regIndex);
-
-    WRITE_REG_32(BCM2711_GPFSEL0 + regIndex,oldGPFSEL | ((function & 0x07) << shiftCount));
+    REG_32(GPSEL_base + regIndex) |= ((function & 0x07) << shiftCount);
 }
 
 void GPIO_SetOutput(uint32_t pin,bool value){
-    volatile uint32_t * Reg = (value == false ? BCM2711_GPCLR0 : BCM2711_GPSET0);
+    volatile uint32_t * Reg = (value == false ? BCM2711_GPIO_GPCLR0 : BCM2711_GPIO_GPSET0);
 
-    if(pin > 31){
-        Reg += 2;
-        pin -= 32;
-    }
-
-    WRITE_REG_32(Reg,(1 << pin));
+    REG_32(Reg + (pin / 32)) = (1 << (pin % 32));
 }
