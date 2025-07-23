169a25da80f44b612dde218415df10b3baf3f3ea
Gordon <bkgr4949p@icloud.com>
2025-07-24 08:59:26 +0900

    Delete temporary debug code in interrupt_handler.c.

--- BarementalOS/src/interrupt_handler.c
+++ BarementalOS/src/interrupt_handler.c
@@ -2,14 +2,10 @@
 #include "HAL_GIC.h"
 #include "interrupt.h"
 
-#include "HAL_GPIO.h"
-
 void IRQ_Handler(){
     uint32_t irqNum = HAL_GIC_GetAcknowledge();
 
-    HAL_GPIO_SetOutput(21, LOW);
     IRQ_CallHandlerCallback(irqNum);
 
     HAL_GIC_EndIRQ(irqNum);
-    return;
 }
