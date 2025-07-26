a2ca15c2ac4f8640c87cbc0ab83ccdd480e353e0
Gordon <bkgr4949p@icloud.com>
2025-07-26 14:12:53 +0900

    Move the IRQ_Handler function to interrupt.c for each architecture.

--- BaremetalOS/src/arch/arm/interrupt.c
+++ BaremetalOS/src/arch/arm/interrupt.c
@@ -1,6 +1,18 @@
+#include <stdint.h>
+
 #include "interrupt.h"
+#include "HAL_GIC.h"
 
 inline void IRQ_Enable(void)
 {
     __asm__ volatile ("cpsie i" : : : "memory");
+}
+
+__attribute__ ((interrupt ("IRQ")))
+void IRQ_Handler(){
+    uint32_t irqNum = HAL_GIC_GetAcknowledge();
+
+    IRQ_CallHandlerCallback(irqNum);
+
+    HAL_GIC_EndIRQ(irqNum);
 }
--- BaremetalOS/src/arch/arm64/interrupt.c
+++ BaremetalOS/src/arch/arm64/interrupt.c
@@ -1,8 +1,19 @@
+#include <stdint.h>
+
 #include "interrupt.h"
+#include "HAL_GIC.h"
 
 void IRQ_Enable(){
     __asm__ volatile(
         "MSR DAIFClr, #2    \n"
         "ISB                \n"
     );
+}
+
+void IRQ_Handler(){
+    uint32_t irqNum = HAL_GIC_GetAcknowledge();
+
+    IRQ_CallHandlerCallback(irqNum);
+
+    HAL_GIC_EndIRQ(irqNum);
 }
