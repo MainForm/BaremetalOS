12f1ed58634a506b0696f54a1cd9cb22f9dafa9b
Gordon <bkgr4949p@icloud.com>
2025-07-28 23:56:31 +0900

    Delete the memory.h file, which contained macros for directly accessing 32-bit registers, as it is no longer needed.

--- BaremetalOS/include/memory.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BM_OS_MEMORY_H__
-#define __BM_OS_MEMORY_H__
-
-#include <stdint.h>
-
-// Read the value from the source address only 32 bits
-#define REG_32(SOURCE)           (*((volatile uint32_t*)SOURCE))
-
-#endif
